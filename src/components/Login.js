28d05f4b30e8adbe0f78fc1ceb1147423a6bf85b
bb2567 <marksong0976@gmail.com>
2021-01-04 19:14:41 +0800

    add style.scss & app.scss. updata login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,32 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
 
 class Login extends Component {
-    render() {
-        return (
-            <div>
-                <p>Login component</p>
+  render() {
+    return ( 
+      <>
+        <div className="login-wrapper">
+          <from className="box login-box">
+            <div className="field">
+              <label className="label">Email</label>
+              <div className="control">
+                <input className="input" type="text" placeholder="Email" />
+              </div>
             </div>
-        );
-    }
+            <div className="field">
+              <label className="label">Password</label>
+              <div className="control">
+                <input className="input" type="text" placeholder="Password" />
+              </div>
+            </div>
+            <div className="control">
+              <button className="button is-fullwidth is-primary">Login</button>
+            </div>
+          </from>
+        </div>
+      </>
+    );
+  }
 }
 
 export default Login;
