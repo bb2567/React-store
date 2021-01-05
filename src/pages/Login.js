4411ea19a42e3766dbca8cf7eac4a077187cdd29
bb2567 <marksong0976@gmail.com>
2021-01-05 23:17:53 +0800

    添加 Refs

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,23 @@
 import React, { Component } from "react";
 
 class Login extends Component {
+
+  // Refs
+  emailRef = React.createRef();
+  passwordRef = React.createRef();
+
   handleSubmit = (event) => {
     // 1.阻止默認行為
     event.preventDefault();
     // 2.取得表單資料
+    const formData = {
+      email: this.emailRef.current.value,
+      password: this.passwordRef.current.value,
+    };
+    console.log(formData)
     // 3.處理登入
     // 4.push切換至首頁
-    this.props.history.push('/')
+    this.props.history.push("/");
   };
   render() {
     return (
@@ -17,13 +27,23 @@ class Login extends Component {
             <div className="field">
               <label className="label">Email</label>
               <div className="control">
-                <input className="input" type="text" placeholder="Email" />
+                <input
+                  className="input"
+                  type="text"
+                  placeholder="Email"
+                  ref={this.emailRef}
+                />
               </div>
             </div>
             <div className="field">
               <label className="label">Password</label>
               <div className="control">
-                <input className="input" type="text" placeholder="Password" />
+                <input
+                  className="input"
+                  type="text"
+                  placeholder="Password"
+                  ref={this.passwordRef}
+                />
               </div>
             </div>
             <div className="control">
