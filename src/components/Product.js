fb2ad0d5c49d7c8f28fde057c8fc56119ccfb8f7
bb2567 <marksong0976@gmail.com>
2021-01-06 00:28:56 +0800

    商品表單組件

---
+++ src/components/Product.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Product extends Component {
+  render() {
+    return (
+      <>
+        <div className="product">
+          <p>Product</p>
+        </div>
+      </>
+    );
+  }
+}
+
+export default Product;
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,10 +1,31 @@
 import React from "react";
+import ToolBox from "./ToolBox";
+import Product from "./Product";
 
 class Products extends React.Component {
   render() {
     return (
-      <div className="products">
-        <p>Products Component</p>
+      <div className="">
+        <ToolBox />
+        <div className="products">
+          <div className="columns is-multiline is-desktop">
+            <div className="column is-3">
+              <Product />
+            </div>
+            <div className="column is-3">
+              <Product />
+            </div>
+            <div className="column is-3">
+              <Product />
+            </div>
+            <div className="column is-3">
+              <Product />
+            </div>
+            <div className="column is-3">
+              <Product />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
