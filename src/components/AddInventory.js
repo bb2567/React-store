f79e87384314af74d2b2bfbe3c7f5cddbdb080bf
bb2567 <marksong0976@gmail.com>
2021-01-07 01:15:41 +0800

    新增商品： form 表單綁定

--- src/components/AddInventory.js
+++ src/components/AddInventory.js
@@ -1,25 +1,107 @@
 import React from "react";
 
 class AddInventory extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      price: "",
+      tags: "",
+      image: "",
+      status: "available",
+    };
+  }
+
+  handelChange = (e) => {
+    const value = e.target.value;
+    const name = e.target.name;
+    this.setState({
+      [name]: value,
+    });
+  };
+  submit = (e) => {
+      e.preventDefault();
+      const product ={...this.state}
+      console.log(product)
+  };
+
   render() {
     return (
       <div className="inventory">
         <p className="title has-text-centered">inventory</p>
-        <br/>
-        <div className="control">
-            <input type="text" className="input"/>
-        </div>
-        <br />
-        <div className="control">
-          <button
-            className="button"
-            onClick={() => {
-              this.props.close('AddInventory Data');
-            }}
-          >
-            Cancel
-          </button>
-        </div>
+        <form onSubmit={this.submit}>
+          <div className="field">
+            <div className="control">
+              <label className="label">Name</label>
+              <textarea
+                className="textarea"
+                name="name"
+                value={this.state.name}
+                onChange={this.handelChange}
+              />
+            </div>
+          </div>
+          <div className="field">
+            <div className="control">
+              <label className="label">Price</label>
+              <input
+                type="number"
+                className="input"
+                name="price"
+                value={this.state.price}
+                onChange={this.handelChange}
+              />
+            </div>
+          </div>
+          <div className="field">
+            <div className="control">
+              <label className="label">Tags</label>
+              <input
+                type="text"
+                className="input"
+                name="tags"
+                value={this.state.tags}
+                onChange={this.handelChange}
+              />
+            </div>
+          </div>
+          <div className="field">
+            <div className="control">
+              <label className="label">Image</label>
+              <input
+                type="text"
+                className="input"
+                name="image"
+                value={this.state.image}
+                onChange={this.handelChange}
+              />
+            </div>
+          </div>
+          <div className="field">
+            <div className="control">
+              <label className="label">Status</label>
+              <div className="select is-fullwidth">
+                <select
+                  name="status"
+                  value={this.state.status}
+                  onChange={this.handelChange}
+                >
+                  <option>available</option>
+                  <option>unavailable</option>
+                </select>
+              </div>
+            </div>
+          </div>
+          <br />
+          <div className="field is-grouped is-grouped-centered">
+            <div className="control">
+              <button className="button is-link">Submit</button>
+            </div>
+            <div className="control">
+              <button className="button ">Cancel</button>
+            </div>
+          </div>
+        </form>
       </div>
     );
   }
