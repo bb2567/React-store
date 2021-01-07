91c9183a0a450e1328fef1be4545b09a661850f1
bb2567 <marksong0976@gmail.com>
2021-01-07 15:56:54 +0800

    添加功能：修改購物車商品

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "../commons/axios";
 import { formatPrice } from "../commons/helper";
 
 const CartItem = (props) => {
-  const { name, image, price, mount } = props.cart || {};
+  //   數量狀態的改變
+  const [mount, setMount] = useState(props.cart.mount);
+  const { id, name, image, price } = props.cart || {};
+  //   小計
   const sumPrice = formatPrice(mount * parseInt(price));
+
+  const handleChang = (e) => {
+    const _mount = parseInt(e.target.value);
+    setMount(_mount);
+    const newCart = { ...props.cart, mount: _mount };
+    axios.put(`/carts/${id}`, newCart).then((res) => {});
+    // 將newCart 傳遞到 Cart的  updateCart()
+    props.updateCart(newCart)
+  };
   return (
     <div className="columns is-vcentered">
       <div className="column is-narrow">
@@ -17,7 +30,13 @@ const CartItem = (props) => {
         <span className="price">{formatPrice(price)}</span>
       </div>
       <div className="column ">
-        <input type="number" className="input num-input" defaultValue={mount} />
+        <input
+          type="number"
+          className="input num-input"
+          min={1}
+          value={mount}
+          onChange={handleChang}
+        />
       </div>
       <div className="column">
         <span className="sum-price">{sumPrice}</span>
