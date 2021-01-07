90cd9d6d0a0cfafd7e3766e9f8528ff5d301cb57
bb2567 <marksong0976@gmail.com>
2021-01-07 20:34:00 +0800

    登入表單驗證

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -26,11 +26,15 @@ export default function Login(props) {
                 placeholder="Email"
                 name="email"
                 ref={register({
-                  required: true,
+                  required: "email is required",
+                  pattern: {
+                    value: /^[A-Za-z0-9]+([_\\.][A-Za-z0-9]+)*@([A-Za-z0-9\\-]+\.)+[A-Za-z]{2,6}$/,
+                    message: "invalid email",
+                  },
                 })}
               />
               {errors.email && (
-                <p className="helper has-text-danger">email is required</p>
+                <p className="helper has-text-danger">{errors.email.message}</p>
               )}
             </div>
           </div>
@@ -43,11 +47,15 @@ export default function Login(props) {
                 placeholder="Password"
                 name="password"
                 ref={register({
-                  required: true,
+                  required: "password is required",
+                  minLength: {
+                    value: 6,
+                    message: "cannot be less than 6 digits",
+                  },
                 })}
               />
-               {errors.password && (
-                <p className="helper has-text-danger">password is required</p>
+              {errors.password && (
+                <p className="helper has-text-danger">{errors.password.message}</p>
               )}
             </div>
           </div>
