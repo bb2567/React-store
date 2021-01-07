c8a3519abd0b7864d049f318b3af9799f6562704
bb2567 <marksong0976@gmail.com>
2021-01-07 22:48:40 +0800

    判斷 jwToken 是否過期

--- src/commons/auth.js
+++ src/commons/auth.js
@@ -3,20 +3,20 @@ const JWT = "store_token";
 
 const setToken = (token) => {
   // 將資料儲存至 localStorage
+
   localStorage.setItem(JWT, token);
 };
 
 const getToken = (token) => {
   return localStorage.getItem(JWT);
 };
-
 // 判斷是否已經登入
 const isLogin= () =>{
   const jwToken = getToken();
-  return !!jwToken
+  return !!jwToken && !isTokenExpired();
 }
 
-
+// 解碼後取得使用者資料
 const getUser = () => {
   const jwToken = getToken();
   if (isLogin()) {
@@ -27,6 +27,20 @@ const getUser = () => {
     return null;
   }
 };
+// -------------- 判斷token 是否過期
+const isTokenExpired = token => {
+  try {
+    const _info = decode(token);
+    if (_info.exp < Date.now() / 1000) {
+      return true;
+    } else return false;
+  } catch (error) {
+    return false;
+  }
+};
+
+
+
 
 // 全域變量
 global.auth = {
