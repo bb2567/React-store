05db321ee79eff57bc656b5eab729e34487604b5
bb2567 <marksong0976@gmail.com>
2021-01-07 14:07:01 +0800

    購物車列表 靜態版型

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,11 +1,22 @@
 import Layout from "Layout";
 import React from "react";
+import CartItem from "../components/CartItem"
 
 const Cart = () => (
   <Layout>
     <div className="cart-page">
-      <p className="title has-text-centered">Cart Page</p>
+      <span className="cart-title">shopping Cart</span>
+      <div className="cart-list">
+        <CartItem />
+        <CartItem />
+        <CartItem />
+      </div>
+      <div className="cart-total">
+        Total:
+        <span className="total-price">2345</span>
+      </div>
     </div>
   </Layout>
 );
 export default Cart;
+ 
